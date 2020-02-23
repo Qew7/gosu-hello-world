5d31a328a59ebca0689a7e35620f6cbe82b44c9a
Maxim <qew7777@gmail.com>
2020-02-23 16:45:13 +0300

    Extract cat fact as game entity

--- main.rb
+++ main.rb
@@ -7,42 +7,26 @@ require_relative 'src/hello_world'
 WIDTH, HEIGHT = 800, 600
 
 class HelloWorld < Gosu::Window
-  attr_reader :cat_api_client
-
   def initialize
     super WIDTH, HEIGHT
 
     self.caption = 'HW: Random Cat Facts'
-    @cat_api_client = CatApiClient.new
     @player = Player.new(self, WIDTH / 2, (HEIGHT / 2) - 30)
+    cat_fact = CatFact.new
     @objects = []
     @objects << @player
+    @objects << cat_fact
   end
 
   def update
     @objects.each { |obj| obj.update }
-    milliseconds = Gosu.milliseconds
-    if Gosu.button_down?(Gosu::KB_RETURN) || @fact.nil? || milliseconds % 1000 == 0
-      update_text
-    end
-    if milliseconds % 500 == 0
+    if Gosu.milliseconds % 500 == 0
       @objects << Cat.new(self, rand(0..HEIGHT), rand(0..WIDTH))
     end
   end
 
   def draw
     @objects.each {|obj| obj.draw }
-    @image_text.draw_rot(WIDTH / 2, HEIGHT / 2, 2, Math.cos(Gosu.milliseconds / 130))
-  end
-
-  private
-
-  def update_text
-    @fact = cat_api_client.cat_fact
-    @image_text = Gosu::Image.from_text((@fact || 'Hello World'), HEIGHT / 15,
-                                        bold: [true, false].sample,
-                                        italic: [true, false].sample,
-                                        width: 399)
   end
 end
 
---
+++ src/game_entities/cat_fact.rb
@@ -0,0 +1,27 @@
+class CatFact < GameEntity
+  attr_reader :cat_api_client
+
+  def initialize
+    @cat_api_client = CatApiClient.new
+  end
+
+  def update
+    if Gosu.button_down?(Gosu::KB_RETURN) || @fact.nil? || Gosu.milliseconds % 1000 == 0
+      update_text
+    end
+  end
+
+  def draw
+    @image_text.draw_rot(WIDTH / 2, HEIGHT / 2, 2, Math.cos(Gosu.milliseconds / 130))
+  end
+
+  private
+
+  def update_text
+    @fact = cat_api_client.cat_fact
+    @image_text = Gosu::Image.from_text((@fact || 'Hello World'), HEIGHT / 15,
+                                        bold: [true, false].sample,
+                                        italic: [true, false].sample,
+                                        width: 399)
+  end
+end
